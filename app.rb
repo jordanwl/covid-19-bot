9897ac6e8c0c7d8a1e54665778c0ff5bba46ff83
Jordan Luong <jordanw.luong@gmail.com>
2020-04-04 01:25:56 +0900

    added location handler and refactored

--- app.rb
+++ app.rb
@@ -5,10 +5,6 @@ require 'line/bot'
 
 Bundler.require
 
-get '/' do
-  "Hello World"
-end
-
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_id = ENV["LINE_CHANNEL_ID"]
@@ -19,30 +15,36 @@ end
 
 post '/callback' do
   body = request.body.read
-
   signature = request.env['HTTP_X_LINE_SIGNATURE']
+
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
 
-  events = client.parse_events_from(body)
-  events.each do |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: get_latest_data(event.message['text'])
-        }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-      end
+  case event.type
+  when Line::Bot::Event::MessageType::Text
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      message = {
+            type: 'text',
+            text: get_latest_data(event.message['text'])
+          }
+
+      client.reply_message(event['replyToken'], message)
     end
+  when Line::Bot::Event::MessageType::Location
+    message = event.message
+
+    reply_content(event, {
+      type: 'location',
+      title: message['title'] || message['address'],
+      address: message['address'],
+      latitude: message['latitude'],
+      longitude: message['longitude']
+    })
   end
+
 
   # Don't forget to return a successful response
   "OK"
@@ -51,6 +53,6 @@ end
 def get_latest_data(prefecture)
   response = HTTParty.get('https://api.apify.com/v2/key-value-stores/YbboJrL3cgVfkV1am/records/LATEST?disableRedirect=true')
 
-  pref_stats = response["infectedByRegion"].select {|obj| obj["region"].downcase === prefecture.downcase }[0]
+  pref_stats = response["infectedByRegion"].select {|obj| obj["region"].downcase === prefecture.downcase.strip }[0]
   "#{pref_stats["region"]}: #{pref_stats["infectedCount"]}"
 end
