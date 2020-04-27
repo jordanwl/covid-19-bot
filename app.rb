fdf207fead583262417f502253853d9e0e7467a6
Jordan Luong <jordanw.luong@gmail.com>
2020-04-27 16:59:59 +0900

    [FIX] refactor and use .include?

--- app.rb
+++ app.rb
@@ -107,15 +107,9 @@ def get_latest_cases(prefecture)
 end
 
 def parse_input_text(text)
-  # check and remove prefectural suffix (-to, -ken, -fu)
-  parsed_text = text.split("").slice(0, (text.length - 1)).join if SUFFIXES.include?(text[-1])
+  PREFECTURES.each { |key, value| return key if value.include?(text.strip) }
 
-  # return english name if in jp else clean input
-  if PREFECTURES.value?(parsed_text)
-    PREFECTURES.key(parsed_text)
-  else
-    text.strip.downcase.gsub('ō', 'o').to_sym
-  end
+  text.strip.downcase.gsub('ō', 'o').to_sym
 end
 
 # constants
@@ -143,54 +137,52 @@ Please try again or text 'help' for more information.
 さらに詳しい情報が必要な場合はhelpと送信してください。
 HEREDOC
 
-SUFFIXES = ['道', '県', '府']
-
 PREFECTURES = {
-  hokkaido: '北海',
-  aomori: '青森',
-  iwate: '岩手',
-  miyagi: '宮城',
-  akita: '秋田',
-  yamagata: '山形',
-  fukushima: '福島',
-  ibaraki: '茨城',
-  tochigi: '栃木',
-  gunma: '群馬',
-  saitama: '埼玉',
-  chiba: '千葉',
+  hokkaido: '北海道',
+  aomori: '青森県',
+  iwate: '岩手県',
+  miyagi: '宮城県',
+  akita: '秋田県',
+  yamagata: '山形県',
+  fukushima: '福島県',
+  ibaraki: '茨城県',
+  tochigi: '栃木県',
+  gunma: '群馬県',
+  saitama: '埼玉県',
+  chiba: '千葉県',
   tokyo: '東京都',
-  kanagawa: '神奈川',
-  niigata: '新潟',
-  toyama: '富山',
-  ishikawa: '石川',
-  fukui: '福井',
-  yamanashi: '山梨',
-  nagano: '長野',
-  gifu: '岐阜',
-  shizuoka: '静岡',
-  aichi: '愛知',
-  mie: '三重',
-  shiga: '滋賀',
-  kyoto: '京都',
-  osaka: '大阪',
-  hyogo: '兵庫',
-  nara: '奈良',
-  wakayama: '和歌山',
-  tottori: '鳥取',
-  shimane: '島根',
-  okayama: '岡山',
-  hiroshima: '広島',
-  yamaguchi: '山口',
-  tokushima: '徳島',
-  kagawa: '香川',
-  ehime: '愛媛',
-  kochi: '高知',
-  fukuoka: '福岡',
-  saga: '佐賀',
-  nagasaki: '長崎',
-  kumamoto: '熊本',
-  oita: '大分',
-  miyazaki: '宮崎',
-  kagoshima: '鹿児島',
-  okinawa: '沖縄'
+  kanagawa: '神奈川県',
+  niigata: '新潟県',
+  toyama: '富山県',
+  ishikawa: '石川県',
+  fukui: '福井県',
+  yamanashi: '山梨県',
+  nagano: '長野県',
+  gifu: '岐阜県',
+  shizuoka: '静岡県',
+  aichi: '愛知県',
+  mie: '三重県',
+  shiga: '滋賀県',
+  kyoto: '京都府',
+  osaka: '大阪府',
+  hyogo: '兵庫県',
+  nara: '奈良県',
+  wakayama: '和歌山県',
+  tottori: '鳥取県',
+  shimane: '島根県',
+  okayama: '岡山県',
+  hiroshima: '広島県',
+  yamaguchi: '山口県',
+  tokushima: '徳島県',
+  kagawa: '香川県',
+  ehime: '愛媛県',
+  kochi: '高知県',
+  fukuoka: '福岡県',
+  saga: '佐賀県',
+  nagasaki: '長崎県',
+  kumamoto: '熊本県',
+  oita: '大分県',
+  miyazaki: '宮崎県',
+  kagoshima: '鹿児島県',
+  okinawa: '沖縄県'
 }
